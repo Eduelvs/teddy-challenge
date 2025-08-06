751ef3e8a59c52c8ad2d77efd8c344674cf21614
Eduelvs <eduardo15022003@gmail.com>
2025-08-06 20:55:27 -0300

    fix: :arrow_up: Upgrading Tailwind dependencies

--- apps/shell/postcss.config.js
+++ apps/shell/postcss.config.js
@@ -1,6 +1,10 @@
+// postcss.config.js (ESM format)
+import postcssPlugin from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    postcssPlugin,
+    autoprefixer,
+  ],
 }
