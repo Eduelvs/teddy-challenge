4f56585d4c849895fb384d8c293e1e00460c3c0e
Eduelvs <eduardo15022003@gmail.com>
2025-08-07 14:28:58 -0300

    style: :art: Functional and stylish sidebar

--- apps/clientes/src/components/Header.tsx
+++ apps/clientes/src/components/Header.tsx
@@ -5,6 +5,8 @@ import Sidebar from './Sidebar'
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false)
+  // Detecta o pathname atual
+  const pathname = window.location.pathname
 
   const toggleSidebar = () => {
     setIsOpen(prev => !prev)
@@ -20,9 +22,30 @@ const Header = () => {
         <img src={teddyLogo} alt="Logo Teddy" className="header-logo" />
         <nav className="header-tabs">
           <ul>
-            <li><a href="/clientes">Clientes</a></li>
-            <li><a href="/selecionados">Clientes Selecionados</a></li>
-            <li><a href="/">Sair</a></li>
+            <li>
+              <a
+                href="/clientes"
+                className={pathname === '/clientes' ? 'active' : ''}
+              >
+                Clientes
+              </a>
+            </li>
+            <li>
+              <a
+                href="/selecionados"
+                className={pathname === '/selecionados' ? 'active' : ''}
+              >
+                Clientes Selecionados
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className={pathname === '/' ? 'active' : ''}
+              >
+                Sair
+              </a>
+            </li>
           </ul>
         </nav>
         <span className="header-username">
